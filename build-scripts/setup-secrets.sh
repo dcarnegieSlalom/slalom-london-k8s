b7ff8067cc478e4800163d2a5eb6933d57cee85a
Donald Carnegie <donald.carnegie@slalom.com>
2018-01-18 13:51:09 +0000

    Remove secrets committed by accident (Now disabled) :)

--- build-scripts/setup-secrets.sh
+++ build-scripts/setup-secrets.sh
@@ -1,7 +1,6 @@
 # Swap out your secrets (do not commit to source control!!!)
 # Run this script in the same directory as  your .travis.yml
 travis encrypt-file build-scripts/large-secrets.sh
-travis encrypt KUBE_PASSWORD="Isb3DlU9uVLsJZ1oKe4HAs6V2qy5Y4HQ" --add
-
-travis encrypt AWS_KEY="AKIAIRNJXXIAE5XUPH3Q" --add
-travis encrypt AWS_SECRET_KEY="8+tsxa0ruB6INuSWtprwiJr99V//JUunOWURqn1e" --add
+travis encrypt KUBE_PASSWORD="[kube password here]" --add
+travis encrypt AWS_KEY="[your aws key here]" --add
+travis encrypt AWS_SECRET_KEY="[your aws secret key here]" --add
