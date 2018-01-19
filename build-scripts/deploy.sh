18fe714bdf84d4b3ac46cc0ebfab90ffc81b45d9
Donald Carnegie <donald.carnegie@slalom.com>
2018-01-19 09:41:29 +0000

    try kubediff

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -2,6 +2,15 @@
 GREEN='\033[0;32m'
 NC='\033[0;0m'
 
-pwd
-ls
-./kubectl get pods
+# Execute terraform init \ terraform plan \ terraform apply on iam role
+
+
+# Execute terraform init \ terraform plan \ terraform apply on prerequisites (the cluster itself)
+
+
+# Execute kubediff and work out what we need to redeploy to the cluster
+kubediff ./k8s-gui
+
+# Redeploy what we need to redeploy.
+
+
