6dedb3f37d47ac196b6d908af20256729cdfca1a
Donald Carnegie <donald.carnegie@slalom.com>
2018-01-22 11:44:49 +0000

    Add log messages to deploy

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -5,20 +5,28 @@ export PATH=$PATH:$(pwd)/kubediff
 export PATH=$PATH:$(pwd)
 
 # Execut kubectl apply on iam role
+echo -e "${GREEN}==== Deploying iam role ====${NC}"
 cd kube2iam
 kubectl apply -f kube2iam-ds.yaml
 kubectl apply -f kube2iam-service-account-role-binding.yaml
 cd ..
+echo -e "${GREEN}==== Done deploying iam role ====${NC}"
+echo ''
 
 # Execute kubectl apply on ingress 
+echo -e "${GREEN}==== Deploying ingress ====${NC}"
 cd ingress
 terraform init
 terraform plan
 terraform apply
 kubectl apply -f alb-ingress-controller.yaml
 kubectl apply -f ingress-service-account-role-binding.yaml
+cd.. 
+echo -e "${GREEN}==== Done deploying ingress ====${NC}"
+echo ''
 
 # Execute kubectl apply on external dns
+echo -e "${GREEN}==== Deploying external dns ====${NC}"
 cd external-dns
 terraform init
 terraform plan
@@ -27,10 +35,15 @@ kubectl apply -f external_dns_test.yaml
 kubectl apply -f external_dns.yaml
 kubectl apply -f external-dns-service-account-role-binding.yaml
 cd ..
+echo -e "${GREEN}==== Done deploying external dns ====${NC}"
+echo ''
 
 # Execute kubectl apply on test application
+echo -e "${GREEN}==== Deploying test app ====${NC}"
 cd hello_app
 kubectl apply -f hello_app_deployment.yaml
 kubectl apply -f hello_app_ingress_resource.yaml
+echo -e "${GREEN}==== Done deploying test app ====${NC}"
+echo ''
 
 
